ed453ee9dbf5b6189cf8cb9d3064d2336ff9a507
Barry Balguissa <balguissabarry43@gmail.com>
2024-02-01 03:36:46 +0100

    indentation

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("form").addEventListener("submit", function (e) {
-        e.preventDefault();
+  document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-        let firstName = document.getElementById("first-name").value;
-        let lastName = document.getElementById("last-name").value;
-        let message = document.getElementById("message").value;
+    let firstName = document.getElementById("first-name").value;
+    let lastName = document.getElementById("last-name").value;
+    let message = document.getElementById("message").value;
 
-        if (firstName === "" || 
-            lastName === "" ||
-            message === "") {
-            document.getElementById("error-message").style.display = "block";
-            return;
-        }
+    if (firstName === "" || lastName === "" || message === "") {
+      document.getElementById("error-message").style.display = "block";
+      return;
+    }
 
-        document.getElementById("error-message").style.display = "none";
+    document.getElementById("error-message").style.display = "none";
 
-        let newMessage = document.createElement("div");
-        newMessage.className = "flex space-x-4 text-sm text-gray-500";
+    let newMessage = document.createElement("div");
+    newMessage.className = "flex space-x-4 text-sm text-gray-500";
 
-        let auteurDiv = document.createElement("div");
-        auteurDiv.className = "flex-1 py-10";
-        let auteurFirstnameLastname = document.createElement("h3");
-        auteurFirstnameLastname.className = "font-medium text-gray-900";
-        auteurFirstnameLastname.textContent = firstName + " " + lastName;
-        
-        auteurDiv.appendChild(auteurFirstnameLastname);
-        let messageConteneur = document.createElement("div");
-        messageConteneur.className = "prose prose-sm mt-4 max-w-none text-gray-500";
-        let messageText = document.createElement("p");
-        messageText.textContent = message;
-        messageConteneur.appendChild(messageText);
+    let auteurDiv = document.createElement("div");
+    auteurDiv.className = "flex-1 py-10";
+    let auteurFirstnameLastname = document.createElement("h3");
+    auteurFirstnameLastname.className = "font-medium text-gray-900";
+    auteurFirstnameLastname.textContent = firstName + " " + lastName;
 
-        newMessage.appendChild(auteurDiv);
-        newMessage.appendChild(messageConteneur);
+    auteurDiv.appendChild(auteurFirstnameLastname);
+    let messageConteneur = document.createElement("div");
+    messageConteneur.className = "prose prose-sm mt-4 max-w-none text-gray-500";
+    let messageText = document.createElement("p");
+    messageText.textContent = message;
+    messageConteneur.appendChild(messageText);
 
-        document.getElementById("comment-list").appendChild(newMessage);
+    newMessage.appendChild(auteurDiv);
+    newMessage.appendChild(messageConteneur);
 
-        document.getElementById("first-name").value = "";
-        document.getElementById("last-name").value = "";
-        document.getElementById("message").value = "";
-    });
+    document.getElementById("comment-list").appendChild(newMessage);
+
+    document.getElementById("first-name").value = "";
+    document.getElementById("last-name").value = "";
+    document.getElementById("message").value = "";
+  });
 });
